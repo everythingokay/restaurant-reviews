f931230d70b4f8e920bb64fda84cfba409b1398f
Alison Feldish <alison.feldish@gmail.com>
2020-10-27 15:27:49 -0400

    print new review form

--- index.js
+++ index.js
@@ -70,6 +70,26 @@ const getReviews = async () => {
     return reviews;
 }
 
+const createReview = async (restaurantId, stars, text, id) => {
+
+    const newReview = {
+        restaurantId,
+        stars,
+        text,
+        id,
+    };
+
+    await fetch("http://localhost:3000/reviews", {
+        method: "POST",
+        body: JSON.stringify(newReview),
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    });
+    getData();
+};
+
 const getData = async () => {
 
     const restaurants = await getRestaurants();
@@ -77,7 +97,7 @@ const getData = async () => {
 
     const result = document.querySelector("#result");
 
-        restaurants.map((restaurant) => {
+        const restaurantData = restaurants.map((restaurant) => {
 
             const reviewData = reviews.filter((review) => {
                 return restaurant.id === review.restaurantId;
@@ -93,56 +113,76 @@ const getData = async () => {
                 averageRating,
             };
 
-        }).sort((a, b) => b.averageRating - a.averageRating).map((restaurant) => {
-
-            const stars = reviews.filter((review) => {
-                return restaurant.id === review.restaurantId;
-            });
-
-            result.innerHTML += 
-            `<div id="post">
-            <div id="name">${restaurant.averageRating}</div>
-            <div id="name">${restaurant.name}</div>
-            <div id="address">${restaurant.address}</div>
-            <div id="image" style="background-image: url(${restaurant.imgUrl})"></div>
-            </div>`
-
-            stars.map((review) => {
-                result.innerHTML += 
-                `<div id="post">
-                <div id="stars">Stars: ${review.stars}</div>
-                <div id="reviews">${review.text}</div>
-                </div>`
-            });
         });
+
+        restaurantData.sort((a, b) => b.averageRating - a.averageRating).map((restaurant) => {
+
+            let resultBox = document.createElement("div");
+            resultBox.classList.add("resultBox");
+            result.appendChild(resultBox);
+
+            resultBox.innerHTML += 
+            `<span id="name">${restaurant.name}</span> - <span id="name">${restaurant.averageRating} ★</span>
+            <div id="address">${restaurant.address}</div>
+            <div id="image" style="background-image: url(${restaurant.imgUrl})" alt="photo of ${restaurant.name}"></div>`
+
+            let reviewBox = document.createElement("div");
+            resultBox.appendChild(reviewBox);
+
+            const stars = reviews.filter((review) => restaurant.id === review.restaurantId);
+
+                stars.forEach((review) => {
+                    reviewBox.innerHTML += 
+                    `<div id="reviews"><span id="star">${review.stars} ★ </span> <span id="text">"${review.text}"</span></div>`
+                });
+
+            let reviewButton = document.createElement("div");
+            reviewButton.id = "reviewButton";
+            reviewButton.classList.add("show");
+            reviewButton.innerHTML = `Write a Review <span id="arrow">↓</span>`;
+            reviewBox.appendChild(reviewButton);
+            reviewButton.addEventListener("click", () => {reviewForm.classList.toggle("show");
+            });
+
+            let reviewForm = document.createElement("form");
+            reviewForm.id = "reviewForm";
+            reviewForm.classList.add("hide");
+            reviewBox.appendChild(reviewForm);
+
+            let starsForm = document.createElement("div");
+            starsForm.id = "starsForm";
+            starsForm.innerHTML = `<span id="stars">★</span>`;
+            reviewForm.appendChild(starsForm);
+
+            let starNumber = document.createElement("input");
+            starNumber.classList.add("starNumber");
+            starNumber.setAttribute("size", "5");
+            starsForm.appendChild(starNumber);
+
+            let textForm = document.createElement("textarea");
+            textForm.classList.add("textForm");
+            textForm.setAttribute("type", "textarea");
+            textForm.setAttribute("maxlength", "2000");
+            textForm.setAttribute("rows", "8");
+            textForm.setAttribute("cols", "62");
+            reviewForm.appendChild(textForm);
+
+            let submitForm = document.createElement("input");
+            submitForm.id = "submitForm";
+            submitForm.setAttribute("type", "submit");
+            submitForm.setAttribute("value", "Submit");
+            reviewForm.appendChild(submitForm);
+
+            const newReview = async (event) => {
+                event.preventDefault();
+                const reviewText = document.getElementsByClassName("textForm")[0].value;
+                const reviewStars = document.getElementsByClassName("starNumber")[0].value;
+                createReview(restaurants.id, parseInt(reviewStars), reviewText);
+                getData();
+            };
+
+            submitForm.addEventListener("click", newReview);
+        })
 };
+
 getData();
-
-
-
-
-
-
-
-
-
-
-// const createRestaurant = async (name, address, imgUrl) => {
-//     await fetch("http://localhost:3000/restaurants", {
-//       method: "POST",
-//       body: JSON.stringify({
-//         name,
-//         address,
-//         imgUrl,
-//       }),
-//       headers: {
-//         Accept: "application/json",
-//         "Content-Type": "application/json",
-//       },
-//     });
-  
-//     console.log("success");
-//   };
-
-
-// (name.value, address.value, text.value, imgsource.value);
